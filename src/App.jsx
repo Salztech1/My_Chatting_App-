982f12cce47eeae55973ec8718b3226c34382b6e
salome <salomenjinda0@gmail.com>
2024-03-05 13:02:32 +0100

    my_chatting_app

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css';
+import React from 'react';
+import {StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import { ChannelContainer, ChannelListContainer, Auth } from './components';
+
+const cookies = new Cookies();
+
+const apiKey = 'wxdvfyhd59tm';
+const authToken = cookies.get('token');
+const client = StreamChat.getInstance(apiKey);
+
+if (authToken) {
+
+  client.connectUser({
+
+    id: cookies.get('userId'),
+    name: cookies.get('username'),
+    fullName: cookies.get('fullName'),
+    image: cookies.get('avatarURL'),
+    hashedPassword: cookies.get('hashedPassword'),
+    phoneNumber: cookies.get('phoneNumber'),
+
+  }, authToken);
+}
+
+
+function App() {
+
+  if (!authToken) return <Auth />
+  return (
+    <div className="app__wrapper">
+
+      <Chat client={client} theme='team light'>
+        <ChannelListContainer
+
+        />
+        <ChannelContainer
+
+        />
+      </Chat>
+
+    </div>
+  );
+}
+
+export default App;
